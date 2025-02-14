4c06c03125159419cf32f0ccd6d8266ca9f445c2
max6555d54f <28157287@populorumjujuy.ar>
2025-02-13 23:47:47 -0300

    prueba

--- app_stock/app_stock.py
+++ app_stock/app_stock.py
@@ -1,33 +1,32 @@
-#https://www.youtube.com/watch?v=h8Tn0ITRoQs
+
+# cuestiones basicas de reflex
+# https://www.youtube.com/watch?v=yFOCSl-OGTE
 # https://www.radix-ui.com/
-# 
+
+
+
 import reflex as rx
 from app_stock.styles.styles import STYLESHEETS, BASE_STYLE # Verifica que estas importaciones sean válidas
-from app_stock.views.navbar import navbar  # Importa correctamente el componente navbar
-from app_stock.views.header import header  # Importa correctamente el componente navbar
-from app_stock.views.footer import footer  # Importa correctamente el componente navbar
-from app_stock.views.specs import specs  # Importa correctamente el componente navbar
+#from app_stock.views.navbar import navbar  # Importa correctamente el componente navbar
+#from app_stock.views.header import header  # Importa correctamente el componente navbar
+#from app_stock.views.footer import footer  # Importa correctamente el componente navbar
+#from app_stock.views.specs import specs  # Importa correctamente el componente navbar
 #from app_stock.views.test import test  # Importa correctamente el componente navbar
-from app_stock.components.background import background_v2  # Importa correctamente el componente navbar
+#from app_stock.components.background import background_v2  # Importa correctamente el componente navbar
 
-from app_stock.pages.index import index 
-from app_stock.pages.demo_front import demo_front
-
+#from app_stock.pages.index import index 
+#from app_stock.pages.demo_front import demo_front
 # Con reflex run --loglevel debug se puede ver el log de la aplicación
-
 class State(rx.State):
     """Define el estado de la aplicación"""
 
+
+    
 # Configuración de la aplicación
 app = rx.App(
     stylesheets=STYLESHEETS,  # Define correctamente tus estilos CSS globales
     style=BASE_STYLE,  # Define un estilo base si es necesario
 )
-
-
-
 # Ejecuta el servidor Reflex
 if __name__ == "__main__":
     app.compile()
-
-
--- rxconfig.py
+++ rxconfig.py
@@ -4,9 +4,11 @@ config = rx.Config(
     app_name="app_stock",
     port=3000,  # Cambia el puerto según tu preferencia
     backend_port=8000,  # Puerto del backend
+    
     cors_allowed_origins=[
-        "http://localhost:3000",
-        "https://app-stock-henna.vercel.app",
+        # dominios que tienen permiso para acceder a tu backend
+        "http://localhost:3000", #frontend local
+        "https://app-stock-henna.vercel.app", #frontend en vercel
     ],
     #esta api es la que se va a usar el front, la misma viene de railway
     #api_url="https://appstock-production-132c.up.railway.app",
